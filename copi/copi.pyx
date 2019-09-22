2a9c40a459b41b7f1ea2c6b6734c88a1b32bff25
Christoph Schueler <cpu12.gems@googlemail.com>
2019-09-22 12:05:12 +0200

    Add SystemInformation

--- copi/copi.pyx
+++ copi/copi.pyx
@@ -1,5 +1,42 @@
 
 # distutils: language=c++
 
-cdef extern from "copi.hpp":
-    pass
+from cpython.bool cimport bool
+cimport libc.stdint as types
+
+ctypedef types.uint32_t DWORD
+
+cdef extern from "copi.hpp" namespace "IOCP":
+
+    cdef cppclass SystemInformation:
+        SystemInformation()
+        DWORD getPageSize()
+        DWORD getAllocationGranularity()
+        DWORD getNumberOfProcessors()
+        bool isWow64()
+
+cdef class SI:
+
+    cdef SystemInformation *_thisptr
+
+    def __cinit__(self):
+        self._thisptr = new SystemInformation()
+        if self._thisptr == NULL:
+            raise MemoryError()
+
+    def __dealloc__(self):
+        if self._thisptr != NULL:
+            del self._thisptr
+
+    cpdef DWORD getPageSize(self):
+        return self._thisptr.getPageSize()
+
+    cpdef DWORD getAllocationGranularity(self):
+        return self._thisptr.getAllocationGranularity()
+
+    cpdef DWORD getNumberOfProcessors(self):
+        return self._thisptr.getNumberOfProcessors()
+
+    cpdef bool isWow64(self):
+        return self._thisptr.isWow64()
+
