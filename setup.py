58064c0f65a13bd34e5779f8842b4863a0897336
Christoph Schueler <cpu12.gems@googlemail.com>
2019-09-24 09:44:50 +0200

    Remove _condition_variable.cpp

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ from Cython.Build import cythonize
 
 extensions = [
     Extension(name = "copi.copi", sources = ["copi/copi.pyx", "copi/_iocp.cpp",
-            "copi/_condition_variable.cpp", "copi/_socket.cpp",
-            "copi/_exceptions.cpp", "copi/_locks.cpp", "copi/_wsock.cpp",
+            "copi/_socket.cpp", "copi/_exceptions.cpp", "copi/_locks.cpp", "copi/_wsock.cpp",
             "copi/_file.cpp", "copi/_mmap.cpp", "copi/_helper.cpp"
         ],
         libraries = ["ws2_32", "kernel32"]),
