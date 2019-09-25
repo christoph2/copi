4f2054c7cb841e9dd746d44cd73822124c37374c
Christoph Schueler <cpu12.gems@googlemail.com>
2019-09-25 15:58:26 +0200

    Bump version: 0.1.17 → 0.1.18

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     tests_require=test_requirements,
     ext_modules = ext_modules,
     url='https://github.com/christoph2/copi',
-    version='0.1.17',
+    version='0.1.18',
     zip_safe=False,
 )
