4b78a88da4ee2a77050023993258cc79811a6b3a
Christoph Schueler <cpu12.gems@googlemail.com>
2019-10-06 11:43:56 +0200

    Add _pool.hpp

---
+++ copi/_pool.hpp
@@ -0,0 +1,71 @@
+/*
+BSD License
+
+Copyright (c) 2019, Christoph Schueler
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef __POOL_HPP
+#define __POOL_HPP
+
+#include <iostream>
+
+#include "copi.hpp"
+
+#include <queue>
+#include <stack>
+
+namespace COPI {
+
+
+template <typename T> class CPool {
+public:
+    CPool(size_t initialSize) {
+        m_pool = new std::queue<T>(initialSize);
+    }
+
+    ~CPool() {
+        if (m_pool != nullptr) {
+            delete m_pool;
+        }
+    }
+
+    void aqcuire() {
+
+    }
+
+    void release() {
+
+    }
+private:
+    std::queue<T> * m_pool;
+};
+
+}
+
+#endif // __POOL_HPP
