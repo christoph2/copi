1dc48c6f5814e013bcb623df63b91eb4ab0cce4a
Christoph Schueler <cpu12.gems@googlemail.com>
2019-12-05 12:36:18 +0100

    Add basic copi tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,4 +1,11 @@
 
+from copi import CSystemInformation
+
+print(si = CSystemInformation())
+print(si.getPageSize())
+print(si.getAllocationGranularity())
+print(si.getNumberOfProcessors())
+print(si.isWow64())
 
 def test_dummy():
     assert 1 == 1
