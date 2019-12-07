1ff01445c766f2d188a145536187db5704ad342c
Christoph Schueler <cpu12.gems@googlemail.com>
2019-12-07 06:56:24 +0100

    Disable tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,12 +1,12 @@
 
 
-from copi import CSystemInformation
+#from copi import CSystemInformation
 
-print(si = CSystemInformation())
-print(si.getPageSize())
-print(si.getAllocationGranularity())
-print(si.getNumberOfProcessors())
-print(si.isWow64())
+#print(si = CSystemInformation())
+#print(si.getPageSize())
+#print(si.getAllocationGranularity())
+#print(si.getNumberOfProcessors())
+#print(si.isWow64())
 
 def test_dummy():
     assert 1 == 1
